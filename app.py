a7dbf2a99e73e222e1dd80bb29fe1b9f2b0eb1c1
SakharovS <sergey@sakharovs.info>
2025-07-27 21:35:57 +0300

    Test autodeploy

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/update', methods=['POST'])
 def update():
